a536be071605f3fafe9e0b5f19fe28fcf843b6e6
qapush <mihail.kapush@gmail.com>
2021-08-17 21:38:32 +0200

    First prototype

--- src/App.js
+++ src/App.js
@@ -4,12 +4,66 @@ import Weather from './services';
 
 export default class App extends Component {
 
-  weather = new Weather();  
+  state = {
+    data: null,
+    loading: true
+  }
+
+  weather = new Weather();
+
+  componentDidMount(){
+    this.updateWeather()
+  }
+  
+  updateWeather(){
+
+
+    this.weather.sixteenDaysForecast()
+        .then(res => console.log(res.data[1]));
+
+    this.weather.sixteenDaysForecast()
+        .then(res => this.setState({
+          data: res.data,
+          loading: false
+        }));
+        
+  }
 
   render(){
+    const { loading } = this.state;
+
     return (
-      <h1>{weather}</h1>
+     <>
+        <h1>Weather</h1>
+        { loading ? null : <List data={ this.state.data } /> }
+     </>
      );
   }
+}
+
+const List = ({data}) => {
+  
+  const days = data.map((item, index) => {
+    return(
+      <div className="card" key={index}>
+        <h3>{item.valid_date}</h3>
+        <hr />
+        <p style={{color:"blue", textAlign:"center"}}>{ item.weather.description }</p>
+        <img src={`${process.env.PUBLIC_URL}/icons/${ item.weather.icon }.png`} alt="icon" />
+        <p>Max temp: <span style={{fontWeight:"bold"}}>{ item.max_temp }</span></p>
+        <p>Min temp: <span style={{fontWeight:"bold"}}>{ item.min_temp }</span></p>
+      </div>
+    )
+  })
+  
+  return(
+    <>
+      <h2>A List:</h2>
+     <div className="cards">
+      { days }
+     </div>
+    </>
+  )
+
 }
 
--- src/services/getWeather.js
+++ src/services/getWeather.js
@@ -1,12 +1,19 @@
 export default class Weather {
     
-    constructor(){
-        this._apiBase = 'https://api.weatherbit.io/v2.0/forecast/daily'
-        this._apiKey = '1ab4251065d442a481c2c53c4ce256da'
-    }
+
+    _apiBase = 'https://api.weatherbit.io/v2.0/forecast/daily'
+    _apiKey = '1ab4251065d442a481c2c53c4ce256da'
 
     async sixteenDaysForecast(){
-        const res = await fetch(`${this._apiBase}?city=Warsaw&country=PL&key=${this._apiKey}`)
+        
+        const res = await fetch(`${this._apiBase}?lang=pl&city=Warsaw&country=PL&key=${this._apiKey}`)
+
+        if(!res.ok){
+            throw new Error(`Could not fetch ${this._apiBase} received ${res.status}`)
+        }
+        
+        return await res.json(); 
+
     }
 
 }
